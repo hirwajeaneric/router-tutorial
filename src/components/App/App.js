74c4fb11553fb966fe99d33005a236f45b1dfd18
hirwajeaneric <hirwajeaneric25@gmail.com>
2022-08-03 13:24:37 +0200

    Step 2 over
    In this step, I added React to my current project. I created a router for each component and I added a navigation using the Link component to swith between routes without a page refresh. In the next step, I will add more complex routes that render different components using URL parameters

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,35 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 
 import Manatee from '../Manatee/Manatee';
+import Narwhal from '../Narwhal/Narwhal';
+import Whale from '../Whale/Whale';
 
 function App() {
   return (
     <div className="wrapper">
       <h1>Marine Mammals</h1>
-      <Manatee />
+      <BrowserRouter>
+        <nav>
+          <ul>
+            <li><Link href='/manatee'>Manatee</Link></li>
+            <li><Link href='/narwal'>Narwhal</Link></li>
+            <li><Link href='/whale'>Whale</Link></li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/manatee">
+            <Manatee />
+          </Route>
+          <Route path="/narwhal">
+            <Narwhal />
+          </Route>
+          <Route path="/whale">
+            <Whale />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
